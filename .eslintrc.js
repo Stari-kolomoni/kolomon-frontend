48edcf1a59366246e94b8b59837365f0f6258f75
Simon Goričar <simon.peter.goricar@gmail.com>
2021-08-30 20:28:10 +0200

    Fix up eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,24 @@
 module.exports = {
     root: true,
     parser: "@typescript-eslint/parser",
-    plugins: [
-        "@typescript-eslint"
-    ],
+    parserOptions: {
+        project: "./tsconfig.json"
+    },
     extends: [
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
         "airbnb-typescript"
     ],
-    parserOptions: {
-        project: "./tsconfig.json"
-    },
+    plugins: [
+        "@typescript-eslint"
+    ],
     rules: {
-        "no-console": "off",
-        indent: ["error", 4],
-        "@typescript-eslint/indent": "off",
-        quotes: ["error", "double"],
-        "@typescript-eslint/quotes": "off",
-        "@typescript-eslint/ban-ts-comment": "off",
-        "max-len": ["error", { comments: 120, code: 90 }],
+        "indent": "off",
+        "@typescript-eslint/indent": ["error", 4],
+        "react/jsx-indent": ["error", 4],
+        "quotes": ["error", "double"],
+        "@typescript-eslint/quotes": ["error", "double"],
+        "max-len": ["warn", { comments: 120, code: 90 }],
+        "react/prefer-stateless-function": "warn"
     },
 };
